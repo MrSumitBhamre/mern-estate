50b4a493c2747f3cc12ebf30f79734856115c766
MrSumitBhamre <sumitbhamre1407@gmail.com>
2026-03-15 10:56:04 +0530

    Clean up db.config.js by removing comments
    Removed comments about MySQL connection pool details.

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,13 +1,7 @@
 const mysql = require('mysql2/promise');
 const env = require('./env.config');
 
-/**
- * MySQL Connection Pool
- * Production ready:
- * - Connection pooling
- * - Queue handling
- * - Prevent connection exhaustion
- */
+
 const pool = mysql.createPool({
   host: env.db.host,
   port: env.db.port,
@@ -15,7 +9,7 @@ const pool = mysql.createPool({
   password: env.db.password,
   database: env.db.name,
   waitForConnections: true,
-  connectionLimit: 20, // Scalable
+  connectionLimit: 20, 
   queueLimit: 0
 });
 
